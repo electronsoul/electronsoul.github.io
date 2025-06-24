ea0c063e42434893f402441c88b064eb2b890f09
electronsoul <satyamkhanna66@gmail.com>
2025-06-25 00:41:08 +0530

    Commit
    Update Globe Path

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,6 @@
 import { rotate } from "maath/buffer";
 import Card from "../components/Card";
-import { Globe } from "../components/globe";
+import { Globe } from "../components/Globe";
 import CopyEmailButton from "../components/CopyEmailButton";
 
 const About = () => {
